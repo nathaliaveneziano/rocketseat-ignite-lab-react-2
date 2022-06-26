f0831efa42609083c69b83ef59cfbb20076f6af5
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-06-26 00:11:05 -0300

    youtube user given cached video fix

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,18 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, DefaultOptions, InMemoryCache } from '@apollo/client';
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
 
 export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ro7vfd1b8901yxc8ay032m/master',
   cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
 });
