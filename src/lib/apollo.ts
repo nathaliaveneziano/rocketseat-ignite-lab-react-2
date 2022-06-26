a265cf530d17b988747bed2f9a3d4bdc374fef4f
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-06-26 13:57:38 -0300

    environment variables added in apollo file

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -12,7 +12,10 @@ const defaultOptions: DefaultOptions = {
 };
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ro7vfd1b8901yxc8ay032m/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
   defaultOptions: defaultOptions,
 });
